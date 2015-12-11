5cbc44b35597d61162b60b249d4b83ac7ed6a867
ZIWEI <GITHUB_USER@users.noreply.github.com>
2015-12-11 10:39:37 -0500

    render mode

--- js/ui.js
+++ js/ui.js
@@ -3,6 +3,9 @@ var UI_cfg = function () {
 
     var controls = {
         gui: null,
+        "Render": 0,
+        "Material":0,
+
         "Cloth Dimension": 50,
         "Particle Mass": 0.1,
         "Time Step": 0.003,
@@ -37,6 +40,14 @@ var UI_cfg = function () {
         var stepEvent = new CustomEvent('step-simulation', null);
         window.dispatchEvent(stepEvent);
     }
+
+    this.getRenderMode = function () {
+        return controls['Render'];
+    };
+
+    this.getRenderMaterial = function () {
+        return controls['Material'];
+    };
 
     this.getRigid = function () {
         return controls['Rigidbody'];
@@ -74,7 +85,6 @@ var UI_cfg = function () {
     this.getKdBend = function () {
         return controls['Kd Bend'];
     };
-
     this.getWindForce = function () {
         if (controls['Wind']) return controls['Wind Force'];
         return 0.0;
@@ -88,7 +98,6 @@ var UI_cfg = function () {
         if (controls['edge 4']) result += 1;
         return result;
     };
-
     this.getPin1 = function () {
         if (controls['pin 1'])
             return 1.0;
@@ -123,6 +132,15 @@ var UI_cfg = function () {
 
         var Simulation_Settings = controls.gui.addFolder('Simulation_Settings');
 
+        Simulation_Settings.add(controls, "Render", {
+            'Points': 0,
+            'Triangle':1,
+        });
+        Simulation_Settings.add(controls, "Material", {
+            'Blinn-Phong': 0,
+            'Debug:Position': 1,
+            'Debug:Normal': 1,
+        });
         Simulation_Settings.add(controls, "Cloth Dimension");
         Simulation_Settings.add(controls, "Particle Mass");
         Simulation_Settings.add(controls, "Time Step");
@@ -143,6 +161,7 @@ var UI_cfg = function () {
         })
         Interaction_Folder.add(controls, "Wind");
         Interaction_Folder.add(controls, "Wind Force");
+
         var EdgeFolder = Interaction_Folder.addFolder('Edges');
         EdgeFolder.add(controls, "edge 1");
         EdgeFolder.add(controls, "edge 2");
@@ -153,9 +172,6 @@ var UI_cfg = function () {
         PinFolder.add(controls, "pin 2");
         PinFolder.add(controls, "pin 3");
         PinFolder.add(controls, "pin 4");
-
-        //Interaction_Folder.open();
-        PinFolder.open();
 
         var Action_Folder = controls.gui.addFolder('Action_Folder');
         Action_Folder.add(controls, "start");
